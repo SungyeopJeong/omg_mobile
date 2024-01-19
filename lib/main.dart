3e66ce9052ad010fefecb158fc38d431c21e95cb
SungyeopJeong <syeopj4a@gmail.com>
2024-01-19 19:28:54 +0900

    feat: connect socket

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:provider/provider.dart';
+import 'package:screen_badminton_mobile/viewmodels/room_model.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  late TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => RoomModel()),
+      ],
+      child: MaterialApp(
+        home: Scaffold(
+          body: Builder(
+            builder: (context) => context.watch<RoomModel>().joined
+                ? const Text('hi')
+                : Column(
+                    children: [
+                      TextField(controller: _controller),
+                      OutlinedButton(
+                        onPressed: () {
+                          final viewModel = context.read<RoomModel>();
+                          viewModel.connect();
+                          viewModel.input(_controller.text);
+                          viewModel.join();
+                        },
+                        child: const Text('JOIN'),
+                      ),
+                    ],
+                  ),
+          ),
         ),
       ),
     );
---
+++ lib/viewmodels/room_model.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:screen_badminton_mobile/services/socket.dart';
+
+class RoomModel extends ChangeNotifier {
+  late SocketService _socketService;
+  SocketService get socketService => _socketService;
+  String? _code;
+  bool joined = false;
+
+  RoomModel() {
+    _socketService = SocketService();
+  }
+
+  void connect() {
+    _socketService.connect();
+  }
+
+  void input(String code) {
+    _socketService.off('ready $_code');
+    _code = code;
+  }
+
+  void join() {
+    _socketService.emit('join', _code);
+    _socketService.on('ready $_code', (data) {
+      joined = data;
+      notifyListeners();
+    });
+  }
+}
